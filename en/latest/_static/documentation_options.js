ce8a1dc23ab3bf0feef9f5cbb65e9fa3f9b7c326
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-23 22:25:25 +0000

    Deploy diofant/diofant.github.io to github.com/diofant/diofant.github.io.git:master

--- en/latest/_static/documentation_options.js
+++ en/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.0a6.dev10+g1145488',
+    VERSION: '0.11.0a6.dev16+gae28b37',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
