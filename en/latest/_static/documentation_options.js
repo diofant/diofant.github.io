77abb5084859d5f9abe6a181360dbb2c7622e7a4
skirpichev <skirpichev@users.noreply.github.com>
2019-11-21 18:28:12 +0000

    Automated deployment: Thu Nov 21 18:28:12 UTC 2019 1fa1812a16e26e405b99f626c00303ce86ce0b21

--- en/latest/_static/documentation_options.js
+++ en/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.0a14.dev95+g3e145f735',
+    VERSION: '0.11.0a14.dev98+g1fa1812a1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
