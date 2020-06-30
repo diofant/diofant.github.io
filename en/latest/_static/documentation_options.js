94a7ee6c24c5e044c0ad6e162b61e6e471905bf3
skirpichev <skirpichev@users.noreply.github.com>
2020-06-30 23:22:13 +0000

    Automated deployment: Tue Jun 30 23:22:13 UTC 2020 0b7efaf7f7da1e4d437e6734e04acfb2aa6b14e1

--- en/latest/_static/documentation_options.js
+++ en/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.1.dev237+g4df001a0e',
+    VERSION: '0.11.1.dev248+g0b7efaf7f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
