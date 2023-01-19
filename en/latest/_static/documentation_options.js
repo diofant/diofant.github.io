a941031c3151bbc73065aba366ae7f0224e8cbda
skirpichev <skirpichev@users.noreply.github.com>
2023-01-19 04:33:58 +0000

    deploy: diofant/diofant@1fa5c62054fb4487a3b5c9321010a13238c0ac0f

--- en/latest/_static/documentation_options.js
+++ en/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.14.0a5.dev37+gb4363f21d',
+    VERSION: '0.14.0a5.dev39+g1fa5c6205',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
