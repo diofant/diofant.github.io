005b2f5cdae3ec0c5ef9715a236b686499516a10
skirpichev <skirpichev@users.noreply.github.com>
2026-01-30 10:03:08 +0000

    deploy: diofant/diofant@23c40ecc2cbc954048cd98bc2452a563f5d90534

--- en/latest/_static/documentation_options.js
+++ en/latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.16.0a3.dev51+g10569509a',
+    VERSION: '0.16.0a3.dev51+g23c40ecc2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
